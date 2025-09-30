c11f7d4f4da05fa1d9ff2c0898b033cad3d0c31c
XiChan <380639414@qq.com>
2025-09-30 14:08:47 +0800

    删除不再使用的 HTML、JavaScript 和 Vue 组件文件，简化项目结构，保留必要的依赖项和配置。

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import {fileURLToPath, URL} from 'node:url'
-
 import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        VueI18nPlugin({ /* options */ }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+    plugins: [],
+    root: 'src',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: resolve(__dirname, 'src/index.html'),
+            output: {
+                entryFileNames: 'index.js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+            }
+        },
+        target: 'es2015',
+        minify: false
+    },
+    esbuild: {
+        target: 'es2015'
     }
 })
